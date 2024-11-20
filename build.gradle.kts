092274c07e299c0fe3fdf946e8732c5d0c9b2fdd
Tullio <46009615+tugaia56@users.noreply.github.com>
2024-11-20 18:06:50 +0100

    Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.6.0")
+        classpath("com.android.tools.build:gradle:8.7.2")
         classpath(kotlin("gradle-plugin", version = Constants.kotlinVersion))
     }
 }
